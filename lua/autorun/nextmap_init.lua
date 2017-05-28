3afc99667bee86be833374637e0da0ca0e31d4f0
ajgeiss0702 <ajgeiss0702@users.noreply.github.com>
2017-05-28 12:50:00 -0700

    same as last one

--- lua/autorun/nextmap_init.lua
+++ lua/autorun/nextmap_init.lua
@@ -1,9 +1,10 @@
-AddCSLuaFile()
 AddCSLuaFile( "nextmap/cl_nextmap.lua" )
+
 if SERVER then
-	include("nextmap/sv_nextmap.lua")
-	AddCSLuaFile( "nextmap/cl_nextmap.lua" )
-	include("nextmap/sv_config.lua")
-else
-	include("nextmap/cl_nextmap.lua")
+    include("nextmap/sv_nextmap.lua")
+    include("nextmap/sv_config.lua")
+end
+
+if CLIENT then
+    include("nextmap/cl_nextmap.lua")
 end
