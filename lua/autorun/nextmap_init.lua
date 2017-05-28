520f3a648b17debb84c554d24c82d2e5b87cd901
ajgeiss0702 <ajgeiss0702@users.noreply.github.com>
2017-05-28 12:28:22 -0700

    Try to fix dedicated servers (again)

--- lua/autorun/nextmap_init.lua
+++ lua/autorun/nextmap_init.lua
@@ -2,6 +2,7 @@ AddCSLuaFile()
 AddCSLuaFile( "nextmap/cl_nextmap.lua" )
 if SERVER then
 	include("nextmap/sv_nextmap.lua")
+	AddCSLuaFile( "nextmap/cl_nextmap.lua" )
 	include("nextmap/sv_config.lua")
 else
 	include("nextmap/cl_nextmap.lua")
