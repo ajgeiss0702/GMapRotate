95dda5ccb30d61a0ce85651d299dcbbf4546d37b
Aiden Geiss <aiden.geiss@gmail.com>
2017-06-19 15:33:28 -0700

    Working timed rotator

--- gmaprotate/lua/autorun/nextmap_init.lua
+++ gmaprotate/lua/autorun/nextmap_init.lua
@@ -7,13 +7,13 @@ print(" ")
 print(" ")
 print(" ")
 print(" ") --]]
-AddCSLuaFile( "nextmap/cl_nextmap.lua" )
+AddCSLuaFile( "gmaprotate/cl_nextmap.lua" )
 
 if SERVER then
 	include("sv_config.lua")
-    include("nextmap/sv_nextmap.lua")
+    include("gmaprotate/sv_nextmap.lua")
 end
 
 if CLIENT then
-    include("nextmap/cl_nextmap.lua")
+    include("gmaprotate/cl_nextmap.lua")
 end
